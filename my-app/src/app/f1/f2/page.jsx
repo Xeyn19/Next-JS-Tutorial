c9fe313568eda0ec92a632614d3d7676bf1e4343
Edgar Jr <edgarrodilorosa@gmail.com>
2025-05-27 15:09:14 +0800

    added intercepting routes ex: (.) , (..) , (...) f1 when refresh it will show the real page

---
+++ my-app/src/app/f1/f2/page.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function F2(){
+    return (
+        <>
+            <h1>F2 page</h1>
+            <div className="">
+                <Link href="/f4">F4</Link>
+            </div>
+        </>
+    )
+
+    
+}
